757efdb256c7352d62b6964cde1023833d08d783
Ian Ozsvald <ian@ianozsvald.com>
2013-04-26 14:02:57 +0100

    minor

--- run_update_all.sh
+++ run_update_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 # Run this using a crontab line like this:
-# */1 * * * * cd /vagrant/bitly_historics_github_vagrant && ./run_update_all.sh
+# */1 * * * * cd /vagrant && ./run_update_all.sh
 source envvagrant/bin/activate
 BITLY_HISTORICS_CONFIG=production python historics.py -e
